a4b7ba50c0215a2c445aec3f1e691729c900ceeb
ratasorin <ratasorin0@gmail.com>
2022-05-29 01:45:31 +0300

    [Feature] Return an acess token on sign in

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,28 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from './interfaces';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
-  async getUser(name: string) {
+  async getUser(name: string): Promise<User> {
     return await this.prisma.user.findFirst({
       where: { name },
     });
   }
 
-  async createUser({ admin, id, name }: User) {
+  async createUser({ admin, id, name, password }: User) {
     try {
       const user = await this.prisma.user.create({
         data: {
           name,
           admin,
           id,
+          password,
         },
       });
 
-      return user;
+      return {
+        access_token: this.jwtService.sign(user),
+      };
     } catch (e) {
       console.error(e);
     }
