c93585fd49843e2eb960c219954d3797b3c76699
ratasorin <ratasorin0@gmail.com>
2022-06-03 23:39:09 +0300

    [Chores] Remove unused MUI components

--- apps/client/src/pages/Building.tsx
+++ apps/client/src/pages/Building.tsx
@@ -1,4 +1,3 @@
-import { Grid } from '@mui/material';
 import { Floor } from '@prisma/client';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
--- apps/client/src/pages/Search.tsx
+++ apps/client/src/pages/Search.tsx
@@ -3,13 +3,12 @@ import jwtDecode from 'jwt-decode';
 import { ReactElement, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import getUser from '../utils/user';
-import SearchBar from 'material-ui-search-bar';
 import { url } from '../constants/server';
 import { Building } from '@shared';
 
 function SearchPage(): ReactElement {
   const navigate = useNavigate();
-  const [buildings, setBuildings] = useState<Building[]>([]);
+  const [Building, setBuilding] = useState<Building[]>([]);
 
   useEffect(() => {
     const user = getUser();
@@ -25,29 +24,28 @@ function SearchPage(): ReactElement {
   }, []);
 
   useEffect(() => {
-    const getAllBuildings = async () => {
-      const response = await fetch(url('buildings'));
-      const buildings = (await response.json()) as Building[];
-      console.log({ buildings });
-      setBuildings(buildings);
+    const getAllBuilding = async () => {
+      const response = await fetch(url('Building'));
+      const Building = (await response.json()) as Building[];
+      console.log({ Building });
+      setBuilding(Building);
     };
 
-    getAllBuildings();
+    getAllBuilding();
   }, []);
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center p-10">
-      <div className="text-3xl font-mono font-light pb-5">
+    <div className="flex h-screen w-screen flex-col items-center p-10">
+      <div className="pb-5 font-mono text-3xl font-light">
         Cauta un loc liber
       </div>
-      <div className="w-3/5 flex flex-col items-center">
-        <SearchBar className="border-4" />
-        {buildings.map(({ name, id, floors }) => (
+      <div className="flex w-3/5 flex-col items-center">
+        {Building.map(({ name, id, floors }) => (
           <button
             key={name}
-            className="shadow-md hover:shadow transition-all py-2 px-5 rounded mt-5"
+            className="mt-5 rounded py-2 px-5 shadow-md transition-all hover:shadow"
             onClick={() =>
-              navigate({ pathname: `buildings/${id}` }, { state: floors })
+              navigate({ pathname: `Building/${id}` }, { state: floors })
             }
           >
             {name}
