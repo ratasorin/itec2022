ff54cbc6f3acd74261e7670f5cb23955e4128689
ratasorin <ratasorin0@gmail.com>
2022-06-04 09:30:28 +0300

    [Refactor] Use hooks for opening and closing widgets
    This way we can close any widgets in the cleanup function of the hook

---
+++ apps/client/src/widgets/utils/useWidgets.ts
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from '../../hooks/redux.hooks';
+import {
+  AvailableWidgets,
+  AvailableWidgetsStateAndProps,
+} from '../store/available';
+
+export const useWidgets = <
+  T extends AvailableWidgetsStateAndProps[AvailableWidgets]
+>(
+  widget: AvailableWidgets
+) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch({
+        type: `${widget}/close`,
+      });
+    };
+  }, []);
+
+  return {
+    open: (payload: Omit<T, 'visible'>) => {
+      dispatch({
+        type: `${widget}/open`,
+        payload: {
+          ...payload,
+          visible: true,
+        },
+      });
+    },
+    close: () => {
+      dispatch({
+        type: `${widget}/close`,
+      });
+    },
+  };
+};
