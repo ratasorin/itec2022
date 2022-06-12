1c59d8fcd7bbe2ff54c40a95800d1c80807af2bb
ratasorin <ratasorin0@gmail.com>
2022-06-13 01:59:13 +0300

    [Feature] Allow user to see all spaces (with bookings) on a floor

--- apps/server/src/floor/floor.controller.ts
+++ apps/server/src/floor/floor.controller.ts
@@ -4,8 +4,13 @@ import { FloorService } from './floor.service';
 @Controller('floor')
 export class FloorController {
   constructor(private floorService: FloorService) {}
-  @Get(':id')
-  getSpaces(@Param('id') id: string) {
-    return this.floorService.getSpaces(id);
+  @Get('/:id/spaces')
+  async getSpaces(@Param('id') id: string) {
+    return await this.floorService.getSpaces(id);
+  }
+
+  @Get('all/:building_id')
+  async getFloors(@Param('building_id') building_id: string) {
+    return await this.floorService.getFloors(building_id);
   }
 }
--- apps/server/src/floor/floor.service.ts
+++ apps/server/src/floor/floor.service.ts
@@ -1,14 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { Floor } from '../../generated/schema';
 import { Pool } from 'pg';
-import { SpacesOnLevel } from '../user/interfaces';
+import { SpacesOnFloor } from '../user/interfaces';
 
 @Injectable()
 export class FloorService {
   constructor(@Inject('CONNECTION') private pool: Pool) {}
   async getSpaces(floor_id: string) {
-    const result = await this.pool.query<SpacesOnLevel>(
+    const result = await this.pool.query<SpacesOnFloor>(
       `--sql
-        SELECT spaces.id AS space_id, x, y, users.name, upper(interval) AS booked_until FROM spaces
+        SELECT spaces.id AS space_id, x, y, users.name AS "occupantName", upper(interval) AS booked_until, spaces.name AS "officeName" FROM spaces
         LEFT JOIN floors ON floors.id = spaces.floor_id
         LEFT JOIN bookings ON bookings.space_id = spaces.id AND bookings.interval @> localtimestamp
         LEFT JOIN users ON bookings.user_id = users.id
@@ -20,5 +21,15 @@ export class FloorService {
 
     const bookingsForFloor = result.rows;
     return bookingsForFloor;
+  }
+
+  async getFloors(building_id: string) {
+    const result = await this.pool.query<Floor[]>(
+      `SELECT * FROM floors WHERE floors.building_id = $1`,
+      [building_id]
+    );
+
+    const floors = result.rows;
+    return floors;
   }
 }
