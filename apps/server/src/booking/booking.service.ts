14f5824b9f8b9bcf67eafa4d42586e5b7a07c9eb
ratasorin <ratasorin0@gmail.com>
2022-06-19 15:20:18 +0300

    [Feature] Fix query sorting the records ascending by id, instead of timestamps
    [Fix] Fix query not selecting all desired intervals

--- apps/server/src/booking/booking.service.ts
+++ apps/server/src/booking/booking.service.ts
@@ -8,22 +8,21 @@ export class BookingService {
   constructor(@Inject('CONNECTION') private pool: Pool) {}
 
   async getTimetable(space_id: string) {
-    console.log({ space_id });
     const response = await this.pool.query<OfficeTimeIntervalDB>(
       `SELECT GREATEST(lower(interval), localtimestamp) AS booked_from, 
       upper(interval) AS booked_until, 
       upper(interval) AS free_from, 
-      lower(lead(interval) OVER (ORDER BY bookings.id)) AS free_until,
+      lower(lead(interval) OVER (ORDER BY bookings.interval)) AS free_until,
       users.name AS "occupantName" 
       FROM bookings 
       LEFT JOIN spaces ON bookings.space_id = spaces.id
       LEFT JOIN users ON bookings.user_id = users.id
-      WHERE spaces.id = $1
+      WHERE spaces.id = $1 AND upper(interval) >= localtimestamp
       `,
       [space_id]
     );
+
     const bookings = response.rows;
-    console.log({ bookings });
 
     return bookings;
   }
@@ -36,7 +35,6 @@ export class BookingService {
     );
 
     const booking = response.rows[0];
-    console.log({ booking });
     return booking;
   }
 }
