23576015792e08c7357274940dec794ab6920f63
ratasorin <ratasorin0@gmail.com>
2022-07-01 15:26:35 +0300

    [Feature] Allow building creation

--- apps/server/src/building/building.controller.ts
+++ apps/server/src/building/building.controller.ts
@@ -1,16 +1,33 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { BuildingService } from './building.service';
+import { JwtUser } from '@shared';
 
 @Controller('building')
 export class BuildingController {
   constructor(private buildingsService: BuildingService) {}
   @Get()
-  async getAllBuilding() {
+  async getAllBuildings() {
     return await this.buildingsService.getBuildings();
   }
 
-  @Get(":building_id/floors")
-  async getFloors(@Param("building_id") building_id: string) {
+  @Get(':building_id/floors')
+  async getFloors(@Param('building_id') building_id: string) {
     return await this.buildingsService.getFloors(building_id);
+  }
+
+  @Post('')
+  @UseGuards(JwtAuthGuard)
+  async createBuilding(@Body('name') name: string, @Request() req) {
+    const user: JwtUser = req.user;
+    return await this.buildingsService.createBuilding(user.id, name);
   }
 }
