6390f7541f190c4b18a23f349c1512d162b30862
ratasorin <ratasorin0@gmail.com>
2023-08-12 12:38:14 +0300

    [Fix] Allow `authorization` header from cross origin requests

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   });
   app.enableCors({
     origin: '*',
-    allowedHeaders: 'Content-Type',
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   app.useGlobalPipes(
