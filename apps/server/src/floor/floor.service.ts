a71f2a8341a119ddc3ca87b857094d0cda5be341
ratasorin <ratasorin0@gmail.com>
2023-08-20 21:19:45 +0300

    [Wip] Improve the logic for retrieving offices

--- apps/server/src/floor/floor.service.ts
+++ apps/server/src/floor/floor.service.ts
@@ -1,43 +1,65 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Pool } from 'pg';
-import { SpacesOnFloor } from '@shared';
+import { OfficesOnFloor } from '@shared';
 import { FloorDB } from '@shared';
 import { FloorUpdateDTO } from './DTO';
 
 @Injectable()
 export class FloorService {
   constructor(@Inject('CONNECTION') private pool: Pool) {}
-  async getSpacesByFloorID(floor_id: string) {
-    const result = await this.pool.query<SpacesOnFloor>(
+  async getOfficesByFloorId(floorId: string) {
+    const result = await this.pool.query<OfficesOnFloor>(
       `--sql
-        SELECT spaces.id AS space_id, x, y, users.name AS "occupantName", upper(interval) AS booked_until, spaces.name AS "officeName" FROM spaces
-        LEFT JOIN floors ON floors.id = spaces.floor_id
-        LEFT JOIN bookings ON bookings.space_id = spaces.id AND bookings.interval @> current_timestamp
+      WITH RECURSIVE block_booking(booking_id, user_id, booked_until) AS (
+        SELECT bookings.id as booking_id, users.id as user_id, upper(bookings.interval) as booked_until FROM offices
+        LEFT JOIN floors ON floors.id = offices.floor_id
+        LEFT JOIN bookings ON bookings.office_id = offices.id AND bookings.interval @> current_timestamp
         LEFT JOIN users ON bookings.user_id = users.id
-        WHERE floors.id = $1
-        ORDER BY space_id;
+        WHERE office.id = '0cc674f4-bfae-468d-8e6f-9f28ffa55053'
+
+        UNION ALL 
+
+        SELECT booking.id, users.id, upper(current_booking.interval) as booked_until FROM block_booking AS previous_booking, bookings AS current_booking
+        LEFT JOIN users ON current_booking.user_id = users.id 
+        WHERE previous_booking.booked_until = lower(current_booking.interval)
+      )
       `,
-      [floor_id]
+      [floorId]
     );
 
     const bookingsForFloor = result.rows;
     return bookingsForFloor;
   }
-  async getSpacesByFloorLevel(building_id: string, floor_level: number) {
-    const result = await this.pool.query<SpacesOnFloor>(
+
+  async getOfficesByFloorLevel(building_id: string, floor_level: number) {
+    const bookedOfficesQuery = await this.pool.query<OfficesOnFloor>(
       `--sql
-        SELECT spaces.id AS space_id, x, y, users.name AS "occupantName", upper(interval) AS booked_until, spaces.name AS "officeName" FROM spaces
-        LEFT JOIN floors ON floors.id = spaces.floor_id
-        LEFT JOIN bookings ON bookings.space_id = spaces.id AND bookings.interval @> current_timestamp
+      SELECT offices.id AS big_office_id, (WITH RECURSIVE block_booking(booking_id, user_id, booked_until, office_id) AS (
+        SELECT bookings.id as booking_id, users.id as user_id, upper(bookings.interval) as booked_until, offices.id AS office_id FROM offices
+        LEFT JOIN bookings ON bookings.office_id = offices.id
         LEFT JOIN users ON bookings.user_id = users.id
-        WHERE floors.building_id = $1 AND floors.level = $2
-        ORDER BY space_id;
+        WHERE offices.id = big_office_id
+          
+          UNION ALL 
+          
+          SELECT current_booking.id AS booking_id, users.id AS user_id, upper(current_booking.interval) as booked_until, previous_booking.office_id FROM block_booking AS previous_booking, bookings AS current_booking
+          LEFT JOIN users ON current_booking.user_id = users.id 
+          WHERE previous_booking.booked_until = lower(current_booking.interval)
+        )
+        SELECT * FROM block_booking ORDER BY block_booking.booked_until DESC LIMIT 1
+      ).user_id FROM offices
+      LEFT JOIN floors ON floors.id = offices.floor_id
+      LEFT JOIN bookings ON bookings.office_id = offices.id
+      LEFT JOIN users ON bookings.user_id = users.id
+      LEFT JOIN buildings ON buildings.id = floors.building_id
+      WHERE buildings.id = $1 AND floors.level = $2 AND bookings.interval @> current_timestamp
       `,
       [building_id, floor_level]
     );
 
-    const bookingsForFloor = result.rows;
-    return bookingsForFloor;
+    const bookedOfficesOnFloor = bookedOfficesQuery.rows;
+
+    return [...bookedOfficesOnFloor];
   }
 
   async createFloor(building_id: string) {
