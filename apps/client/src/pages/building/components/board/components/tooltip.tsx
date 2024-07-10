932dbc1bf2bdc42ee938d12f300ab096dd3b5d96
ratasorin <ratasorin0@gmail.com>
2024-07-10 17:08:45 +0300

    [Feature] Improve the design of the editing tooltip

--- apps/client/src/pages/building/components/board/components/tooltip.tsx
+++ apps/client/src/pages/building/components/board/components/tooltip.tsx
@@ -5,9 +5,19 @@ import useHandleClickOutside from '@client/hooks/click-outside';
 import { popupStateMachine } from './popups';
 import { Box } from '@client/pages/timetable/widgets/picker-popup/picker.slice';
 import { TwitterPicker } from 'react-color';
-import { Button } from '@mui/material';
+import {
+  Button,
+  ClickAwayListener,
+  Divider,
+  IconButton,
+  TextField,
+} from '@mui/material';
 import { modifyShapeModalAtom } from '../edit';
 import { INITIAL_FILL_COLOR } from '../constants';
+import { LuTextCursorInput } from 'react-icons/lu';
+import { Tooltip as TooltipMUI } from '@mui/material';
+import { LuShapes } from 'react-icons/lu';
+import { strokeColorBasedOnFill } from '../utils/draggable-node';
 
 export const colorAtom = atom<string | null>(INITIAL_FILL_COLOR);
 export const nodeKeyAtom = atom<go.Key | null>(null);
@@ -33,18 +43,31 @@ const Tooltip: FC<{
   useHandleClickOutside('desk-tooltip', closeDeskTooltip, render);
   const setModalOpen = useSetAtom(modifyShapeModalAtom);
 
+  const [openColorPicker, setOpenColorPicker] = useState(false);
+
   useEffect(() => {
     if (!tooltip.current || !box) return setDimensions([null, null]);
     const dimensions = tooltip.current.getBoundingClientRect();
 
     const { height: tooltipHeight, width: tooltipWidth } = dimensions;
-    const { left: leftBox, top: topBox, width: widthBox } = box;
+    const {
+      left: leftBox,
+      top: topBox,
+      width: widthBox,
+      height: heightBox,
+    } = box;
 
     let left = leftBox + widthBox / 2 - tooltipWidth / 2;
     let top = topBox - tooltipHeight - 10;
 
-    if (left <= 0) left = 0.1;
-    if (top <= 0) top = 0.1;
+    if (top <= 0 || left <= 0) {
+      left = leftBox + widthBox + 10;
+      top = topBox + heightBox / 2 - tooltipHeight / 2;
+    }
+    if (leftBox + widthBox + 10 + tooltipWidth > window.innerWidth) {
+      left = leftBox - tooltipWidth - 10;
+      top = topBox + heightBox / 2 - tooltipHeight / 2;
+    }
 
     setDimensions([left, top]);
   }, [box]);
@@ -53,34 +76,96 @@ const Tooltip: FC<{
     <div
       id="desk-tooltip"
       ref={tooltip}
-      className="font-poppins absolute z-50 flex flex-col items-center rounded-md bg-white p-4 text-lg font-black shadow-md"
+      className="font-poppins absolute z-50 flex flex-row items-center rounded-md border border-slate-300 bg-white p-1 text-lg font-black shadow-md"
       style={{
         visibility: render ? 'visible' : 'hidden',
         top: top || 0,
         left: left || 0,
       }}
     >
-      <Button
-        variant="outlined"
-        className="font-poppins row-start-2 mb-3 border-black text-black hover:border-black hover:bg-black/5"
-        onClick={() => {
-          console.log({ color });
-          setModalOpen({
-            render: true,
-            nodePath,
-            color: color ? color : undefined,
-          });
+      <TooltipMUI title="EDIT THE NAME">
+        <IconButton className="rounded-lg p-2 text-gray-800">
+          <LuTextCursorInput />
+        </IconButton>
+      </TooltipMUI>
+
+      <Divider
+        orientation="vertical"
+        variant="middle"
+        flexItem
+        className="mx-1"
+      />
+
+      <TooltipMUI title="CUSTOMIZE SHAPE">
+        <IconButton className="rounded-lg p-2 text-gray-800">
+          <LuShapes
+            onClick={() =>
+              setModalOpen({
+                render: true,
+                color: color || undefined,
+                nodePath,
+              })
+            }
+          />
+        </IconButton>
+      </TooltipMUI>
+
+      <Divider
+        orientation="vertical"
+        variant="middle"
+        flexItem
+        className="mx-1"
+      />
+
+      <ClickAwayListener
+        onClickAway={() => {
+          setOpenColorPicker(false);
         }}
       >
-        RESHAPE
-      </Button>
-      <TwitterPicker
-        triangle="hide"
-        onChange={(color) => {
-          console.log({ hex: color.hex });
-          setColor(color.hex);
-        }}
-      />
+        <div>
+          <TooltipMUI
+            PopperProps={{
+              disablePortal: true,
+            }}
+            onClose={() => {
+              setOpenColorPicker(false);
+            }}
+            componentsProps={{
+              popper: { className: 'm-0' },
+              tooltip: { className: 'm-0 !mt-1 bg-transparent' },
+            }}
+            open={openColorPicker}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            title={
+              <TwitterPicker
+                triangle="hide"
+                onChange={(color) => {
+                  console.log({ hex: color.hex });
+                  setColor(color.hex);
+                }}
+              />
+            }
+          >
+            <Button
+              onClick={() => setOpenColorPicker(true)}
+              className="font-poppins flex items-center rounded-lg border-black p-2 font-semibold capitalize text-black hover:border-black hover:bg-black/5"
+            >
+              <span className="mr-2 text-base">Color</span>
+              <div
+                className="h-6 w-6 rounded-md border-2"
+                style={{
+                  backgroundColor: color || INITIAL_FILL_COLOR,
+                  borderColor: strokeColorBasedOnFill(
+                    color || INITIAL_FILL_COLOR
+                  ),
+                }}
+              ></div>
+            </Button>
+          </TooltipMUI>
+        </div>
+      </ClickAwayListener>
     </div>,
     document.body
   );
